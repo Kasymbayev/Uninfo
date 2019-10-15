43709f88ebb7e9a5d8c7612039d2d750a1b25fe0
kassymbayev <kasymbaayev@gmail.com>
2019-10-15 15:19:14 +0600

    Первая загрузка файлов

---
+++ database/migrations/2019_10_13_160628_create_specialty_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSpecialtyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('specialty', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('university_id');
+            $table->foreign('university_id')->references('id')->on('university');
+            $table->string('specialty_name',25);
+            $table->string('specialty_cipher',8);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('specialty');
+    }
+}
---
+++ database/migrations/2019_10_13_201020_create_price_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePriceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('price', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('university_id');
+            $table->foreign('university_id')->references('id')->on('university');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('price');
+    }
+}
